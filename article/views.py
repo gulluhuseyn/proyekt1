7170745838c29e31710a28a3e3a341aa3c4d628a
gulluhuseyn <gulluhuseynova2@gmail.com>
2024-06-04 13:34:42 +0400

    baslangic

---
+++ article/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from .models import Article
+# Create your views here.
+def home_view(request):
+    articles = Article.objects.all()
+    return render(request, "index.html" ,{'articles':articles})
+def about_view(request):
+    return render(request, "about.html")
+def contact_view(request):
+    return render(request, "contact.html")
